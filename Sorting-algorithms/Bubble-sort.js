05017c98a5b7c94ae0fde36fa9c7a303c1852707
Itzik Shaoulian <itzikshaoulian@gmail.com>
2019-07-19 14:33:17 -0400

    adding different solutions

--- Sorting-algorithms/Bubble-sort.js
+++ Sorting-algorithms/Bubble-sort.js
@@ -1,20 +1,55 @@
-//Implementation of Bubble-sort algorithm
+// //Implementation of Bubble-sort algorithm
 
-const bubbleSort = arr => {
-    let swapped;
-    do{
-        swapped = false;
-        for(let i = 0; i < arr.length; i++) {
-            if(arr[i] > arr[i+1]) {
-                let tmp = arr[i];
-                arr[i] = arr[i+1];
+// const bubbleSort = arr => {
+//     let swapped;
+//     do{
+//         swapped = false;
+//         for(let i = 0; i < arr.length; i++) {
+//             if(arr[i] > arr[i+1]) {
+//                 let tmp = arr[i];
+//                 arr[i] = arr[i+1];
+//                 arr[i+1] = tmp;
+//                 swapped = true;
+//             }
+//         }
+//     }while(swapped);
+//     return arr;
+// }
+
+// console.log(bubbleSort([4,2,1,3]))
+
+//Descending order
+let bubbleSort = arr => {
+    let bool = true;
+    while(bool) {
+        bool = false;
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] < arr[i+1]){
+                tmp = arr[i]
+                arr[i] = arr[i+1]
                 arr[i+1] = tmp;
-                swapped = true;
+                bool = true;
             }
         }
-    }while(swapped);
-
+    }
     return arr;
 }
 
-//[2,1,4,3] -> [1,2,3,4]
+//Increasing order
+let bubbleSortg = arr => {
+    let bool = true;
+    while(bool) {
+        bool = false;
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] > arr[i+1]){
+                tmp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = tmp;
+                bool = true;
+            }
+        }
+    }
+    return arr;
+}
+
+// console.log(bubbleSortg([4,2,1,3,7,4,8,9,5,2,3]))
